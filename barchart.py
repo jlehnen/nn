953d95355bed7b8b1b5087ee3e48283af3e804ce
julian.lehnen <julian.lehnen@traveltainment.de>
2019-10-31 16:37:06 +0100

    barchart for plotting activations

---
+++ barchart.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, axes = plt.subplots(2, 4)
+activations = np.random.rand(8, 3)*2
+
+
+def plot_activations(activations):
+    for i in range(len(activations)):
+        y_pos = np.arange(len(activations[i]))
+        a = 0 if i < 4 else 1
+        b = i % 4
+        axes[a, b].barh(y_pos, activations[i], align='center')
+        axes[a, b].set_yticks(y_pos)
+        axes[a, b].set_yticklabels(y_pos + 1)
+        # axes[a, b].set_xlabel('Activation')
+        axes[a, b].set_ylabel('Neuron')
+        axes[a, b].set_title('Example {}'.format(i + 1))
+
+    fig.set_size_inches(10, 6)
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    fig.suptitle('Activations')
+    fig.savefig('activations.pdf', format="pdf", bbox_inches='tight')
+    plt.show()
+
+
+plot_activations(activations)
